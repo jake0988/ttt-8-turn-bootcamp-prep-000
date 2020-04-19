ae306711860f0bef1aab7de02e4fcbaabf131e2d
Jake G <jake098877@gmail.com>
2020-04-19 13:14:57 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+def display_board(board)
+  print board
+end
+
+def valid_move(board, index)
+  if (index.between?(0, 8) && board[index] != 'X' || board[index] != 'O')
+    true
+end
+
+def move(board, index, token = 'X')
+  board[index] = token
+
+end
