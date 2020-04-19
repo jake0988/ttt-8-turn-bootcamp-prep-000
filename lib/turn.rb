738bbe3cb72982a38b348daa06cdf029211b678c
Jake G <jake098877@gmail.com>
2020-04-19 13:16:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,7 @@ def valid_move(board, index)
   if (index.between?(0, 8) && board[index] != 'X' || board[index] != 'O')
     true
 end
+end
 
 def move(board, index, token = 'X')
   board[index] = token
