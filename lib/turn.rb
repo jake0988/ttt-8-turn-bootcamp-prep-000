452e82b48c9f527ae6f997f3ed4d7ada7bd0fbeb
Jake G <jake098877@gmail.com>
2020-04-19 13:41:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ end
 end
 
 def valid_move?(board, index)
-  if (index.between?(0, 8) && !position_taken?)
+  if (index.between?(0, 8) && !position_taken?(board, index))
     true
   else
     false
