dcedc4524f73f24fadd87b5cce3532a45987c05e
Jake G <jake098877@gmail.com>
2020-04-19 15:27:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    while !input.between?(1, 9)
+    if (!input.between?(1, 9))
        turn(board)
       if (valid_move?(board, index) == true)
         move(board, index)
